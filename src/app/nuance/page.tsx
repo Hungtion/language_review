ad2b465ffbb1a8f92db2fd72ecf4f8621167cfae
SunghyunKim-neolab <kimsunghyun@neolab.net>
2026-06-16 18:14:24 +0900

    Revert "Fix Safari touch freeze: use overflow hidden on html/body instead of position fixed"
    This reverts commit e898196dad4b194d8ee294b2fa989d096575a8ec.

--- src/app/nuance/page.tsx
+++ src/app/nuance/page.tsx
@@ -102,15 +102,13 @@ function NuanceContent() {
 
   // Lock body scroll
   useEffect(() => {
-    document.documentElement.style.overflow = "hidden";
     document.body.style.overflow = "hidden";
-    document.documentElement.style.height = "100%";
-    document.body.style.height = "100%";
+    document.body.style.position = "fixed";
+    document.body.style.width = "100%";
     return () => {
-      document.documentElement.style.overflow = "";
       document.body.style.overflow = "";
-      document.documentElement.style.height = "";
-      document.body.style.height = "";
+      document.body.style.position = "";
+      document.body.style.width = "";
     };
   }, []);
 
